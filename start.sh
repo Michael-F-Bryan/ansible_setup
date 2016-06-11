b069968968c12b607c8d66d031a85ee31d37079c
Michael Bryan <michaelfbryan@gmail.com>
2016-06-12 00:44:27 +0800

    Edited the start script

--- start.sh
+++ start.sh
@@ -1,39 +1,47 @@
 #!/bin/bash
 
+# Miscellaneous functions
 function usage() {
-  echo "Usage: $0 [-h]"
+  echo "Usage: $0 [-h] [playbook_name]"
 }
 
 function print_help() {
   usage
   echo
-  echo "-h     Print out this help text"
+  echo "Options:"
+  echo "-h                 Print out this help text"
+  echo
+  echo "Arguments:"
+  echo "playbook_name      Name of the playbook to run (defaults to \"main\")"
 }
 
 # Initialize variables
-OPTIND=0
+OPTIND=1
 
-# Parse commandline arguments
+# Parse commandline arguments with getopts
 while getopts ":h" opt; do
   case ${opt} in
     h )
-      print_help
+      print_help >&2
       exit 0
       ;;
     \? )
-      usage
+      echo "Invalid option: -$OPTARG" >&2
+      usage >&2
       exit 1
       ;;
   esac
 done
 shift $((OPTIND-1))
 
-# If there were any left over arguments then print
-# out usage and exit
-if [[ $(($# - OPTIND)) >  0 ]]; then
-  usage
+# If there are any other arguments, print usage and exit
+if [[ $# > 0 ]]; then
+  usage >&2
   exit 1
 fi
 
-# Play the master playbook
+# Run the command
+echo "Running command: ansible-playbook site.yml --ask-vault-pass"
 ansible-playbook site.yml --ask-vault-pass
+exit $?
+
